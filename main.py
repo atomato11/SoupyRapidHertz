5b15f3016f77a94b14ada682d3ef1038c3d068b2
sundaytajt <96949183+atomato11@users.noreply.github.com>
2022-01-18 22:45:42 +0000

    else

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ if activated:
   print("Hello World!")
 if not activated:
   print("Good Bye World!")
+response = input("What is your reaction to this")
+if response == "sad":
+    print("sorry")
+else:
+    print("bruh")
